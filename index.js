e64e34652910243dc9d1e679e2071ef1fcc36ed5
rahimshahad <rahim.shahad2@gmail.com>
2021-09-06 17:00:56 -0400

    commit tree 973e230b0354aec86dfd66c558e2d9e5b91e0b53

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+const num1 = 4;
+const num2 = 15.5;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() + 1);
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+const max = Math.max(1, 8, 3, 20);
